fe236957a98cf97c7b693c9deedb0719900e5707
Marco Pensallorto <marco.pensallorto@gmail.com>
2018-03-14 00:35:10 +0100

    Solved #168

--- forclojure.clj
+++ forclojure.clj
@@ -3187,7 +3187,8 @@
                    (clojure.set/union ps (map #(conj % (first lst)) ps)))))
 
              (sums [coll]
-               (vec (sort (distinct (map (partial reduce +) (remove empty? (powerset coll)))))))
+               (vec (sort (distinct (map (partial reduce +)
+                                         (remove empty? (powerset coll)))))))
 
              (common-elem [& colls]
                (if (some empty? colls) nil
@@ -4000,6 +4001,91 @@
    (= :eq (__ (fn [x y] (< (count x) (count y))) "pear" "plum"))
    (= :lt (__ (fn [x y] (< (mod x 5) (mod y 5))) 21 3))
    (= :gt (__ > 0 2))))
+
+;; 168. Infinite Matrix
+
+;;  In what follows, m, n, s, t denote nonnegative integers, f denotes
+;;  a function that accepts two arguments and is defined for all
+;;  nonnegative integers in both arguments.
+
+;; In mathematics, the function f can be interpreted as an infinite
+;; matrix with infinitely many rows and columns that, when written,
+;; looks like an ordinary matrix but its rows and columns cannot be
+;; written down completely, so are terminated with ellipses. In
+;; Clojure, such infinite matrix can be represented as an infinite
+;; lazy sequence of infinite lazy sequences, where the inner sequences
+;; represent rows.
+
+;; Write a function that accepts 1, 3 and 5 arguments
+
+;; 1. with the argument f, it returns the infinite matrix A that has the
+;; entry in the i-th row and the j-th column equal to f(i,j) for i,j =
+;; 0,1,2,...;
+
+;; 2. with the arguments f, m, n, it returns the infinite matrix B that
+;; equals the remainder of the matrix A after the removal of the first
+;; m rows and the first n columns;
+
+;; 3. with the arguments f, m, n, s, t, it returns the finite s-by-t
+;; matrix that consists of the first t entries of each of the first s
+;; rows of the matrix B or, equivalently, that consists of the first s
+;; entries of each of the first t columns of the matrix B.
+
+(let [__
+      (fn aux
+        ([f] (aux f 0 0))
+        ([f m n] (aux f m n Integer/MAX_VALUE Integer/MAX_VALUE))
+        ([f m n s t]
+         (letfn
+             [(outer [r]
+                (letfn [(inner [c]
+                          (if (< c (+ t n))
+                            (lazy-seq
+                             (cons (f r c)
+                                   (inner (inc c))))))]
+                  (if (< r (+ s m))
+                    (lazy-seq
+                     (cons (inner n)
+                           (outer (inc r)))))))]
+           (outer m))))]
+  (and
+   (= (take 5 (map #(take 6 %) (__ str)))
+      [["00" "01" "02" "03" "04" "05"]
+       ["10" "11" "12" "13" "14" "15"]
+       ["20" "21" "22" "23" "24" "25"]
+       ["30" "31" "32" "33" "34" "35"]
+       ["40" "41" "42" "43" "44" "45"]])
+
+   (= (take 6 (map #(take 5 %) (__ str 3 2)))
+      [["32" "33" "34" "35" "36"]
+       ["42" "43" "44" "45" "46"]
+       ["52" "53" "54" "55" "56"]
+       ["62" "63" "64" "65" "66"]
+       ["72" "73" "74" "75" "76"]
+       ["82" "83" "84" "85" "86"]])
+
+   (= (__ * 3 5 5 7)
+      [[15 18 21 24 27 30 33]
+       [20 24 28 32 36 40 44]
+       [25 30 35 40 45 50 55]
+       [30 36 42 48 54 60 66]
+       [35 42 49 56 63 70 77]])
+
+   (= (__ #(/ % (inc %2)) 1 0 6 4)
+      [[1/1 1/2 1/3 1/4]
+       [2/1 2/2 2/3 1/2]
+       [3/1 3/2 3/3 3/4]
+       [4/1 4/2 4/3 4/4]
+       [5/1 5/2 5/3 5/4]
+       [6/1 6/2 6/3 6/4]])
+
+   (= (__ #(/ % (inc %2)) 1 0 6 4)
+      [[1/1 1/2 1/3 1/4]
+       [2/1 2/2 2/3 1/2]
+       [3/1 3/2 3/3 3/4]
+       [4/1 4/2 4/3 4/4]
+       [5/1 5/2 5/3 5/4]
+       [6/1 6/2 6/3 6/4]])))
 
 ;; 171. Intervals
 
